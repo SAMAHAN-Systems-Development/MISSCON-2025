ffc2bfa41c72aa12ada3970cae93e90db3365bed
John Kyle Lampa <jklampa31@gmail.com>
2024-12-18 22:15:48 +0800

    style: minor improvement to transitions

--- src/components/ui/MenuNavButtons.tsx
+++ src/components/ui/MenuNavButtons.tsx
@@ -7,20 +7,25 @@ interface ButtonProps {
 
 export default function MenuNavButtons({ text, href }: ButtonProps) {
   return (
-    <div
-      className="
-        flex items-center font-inandan text-white relative w-full sm:w-9/12
-        text-base h-10 pl-8
-        sm:text-xl sm:h-11 sm:pl-10
-        md:text-2xl md:h-12 md:pl-14
-        lg:text-3xl lg:h-14 lg:pl-16
-        xl:text-4xl xl:h-16 xl:pl-20
-        hover:bg-gradient-to-r hover:from-white hover:text-light-violet hover:from-60% hover:sm:from-30%
-        "
+    <a
+      href={href}
+      className="relative group w-full sm:w-9/12 h-10 sm:h-11 md:h-12 lg:h-14 xl:h-16 block"
     >
-      <a className="w-full block" href={href}>
+      <div
+        className="
+          absolute inset-0 bg-gradient-to-r from-white to-transparent transition-opacity duration-300 opacity-0
+          group-hover:opacity-100
+        "
+      ></div>
+      <div
+        className="
+          relative h-full flex items-center font-inandan text-white text-base pl-8
+          sm:text-xl sm:pl-10 md:text-2xl md:pl-14 lg:text-3xl lg:pl-16
+          xl:text-4xl xl:pl-28 group-hover:text-light-violet transition-colors duration-300
+        "
+      >
         {text}
-      </a>
-    </div>
+      </div>
+    </a>
   );
 }
