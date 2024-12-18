72b6a71c7317986d65ab04a55af5cd8f2b429f78
John Kyle Lampa <jklampa31@gmail.com>
2024-12-18 16:04:03 +0800

    style: added minor improvements

--- src/components/ui/RuleCards.tsx
+++ src/components/ui/RuleCards.tsx
@@ -2,22 +2,23 @@
 import React from 'react';
 import rulesData from '@/data/rules.json';
 
-
 const RuleCards = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 gap-y-20 w-full max-w-6xl">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-y-20 gap-x-10 w-max">
         {rulesData.map((rule, index) => (
           <div
             key={index}
-            className="flex flex-col hover:bg-light-violet text-center bg-gradient-light-violet items-center rounded-2xl p-6 shadow-xl max-w-lg h-[220px] mx-4"
+            className="flex flex-col hover:bg-light-violet text-center bg-gradient-light-violet transition-all duration-200 items-center rounded-2xl py-5 px-4 shadow-xl w-[473px] h-[225px]"
           >
-            <header className="text-white font-inandan text-3xl font-medium mb-4">
+            <header className="text-white font-inandan text-3xl font-medium">
               {rule.heading}
             </header>
-            <article className="text-white font-gill_sans font-base text-lg overflow-auto">
-              {rule.content}
-            </article>
+            <div className="flex flex-col justify-center h-full">
+              <article className="text-white font-gill_sans font-base font-medium text-lg leading-6 overflow-auto">
+                {rule.content}
+              </article>
+            </div>
           </div>
         ))}
       </div>
