9e9ea2a7dc4b7bcf04ba1865975912d09133a2b3
John Kyle Lampa <jklampa31@gmail.com>
2024-12-18 22:39:42 +0800

    feat: added menu button

--- src/components/ui/MenuButton.tsx
+++ src/components/ui/MenuButton.tsx
@@ -11,34 +11,43 @@ export default function MenuButton({ active, onClick }: MenuButtonProps) {
 
   const menuButton = '/images/MenuButton.png';
   const menuButtonHovered = '/images/MenuButton-Hovered.png';
+  const menuButtonActive = '/images/MenuButton-Active.png';
 
   return (
     <div
-      className="relative w-20 h-20 overflow-hidden group hover:cursor-pointer"
+      className="relative w-20 h-20 overflow-hidden hover:cursor-pointer"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       onClick={onClick}
     >
-      {/* Default Image */}
       <Image
         draggable={false}
         fill
         src={menuButton}
-        className={`w-min h-min rounded-card object-cover object-center absolute inset-0 transition-opacity duration-300 ${
+        className={`absolute inset-0 w-min h-min rounded-card object-cover object-center transition-opacity duration-300 ${
           active || isHovered ? 'opacity-0' : 'opacity-100'
         }`}
         alt="Menu Button"
       />
 
-      {/* Hovered/Active Image */}
       <Image
         draggable={false}
         fill
         src={menuButtonHovered}
-        className={`w-min h-min rounded-card object-cover object-center absolute inset-0 transition-opacity duration-300 ${
-          active || isHovered ? 'opacity-100' : 'opacity-0'
+        className={`absolute inset-0 w-min h-min rounded-card object-cover object-center transition-opacity duration-300 ${
+          active || !isHovered ? 'opacity-0' : 'opacity-100'
         }`}
         alt="Menu Button Hovered"
+      />
+
+      <Image
+        draggable={false}
+        fill
+        src={menuButtonActive}
+        className={`absolute inset-0 w-min h-min rounded-card object-cover object-center scale-[0.72] mt-[-2.5px] ${
+          active ? 'opacity-100' : 'opacity-0'
+        }`}
+        alt="Menu Button Active"
       />
     </div>
   );
