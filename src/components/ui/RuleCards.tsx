dcc40a417b402774344cce661a47c583affd5556
Ninya <mnamparaiso@addu.edu.ph>
2024-12-18 14:41:00 +0800

    feat: added hover style, fix margins and size of cards, interchanged data for the rules.

--- src/components/ui/RuleCards.tsx
+++ src/components/ui/RuleCards.tsx
@@ -6,16 +6,16 @@ import rulesData from '@/data/rules.json';
 const RuleCards = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 w-full max-w-6xl">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 gap-y-20 w-full max-w-6xl">
         {rulesData.map((rule, index) => (
           <div
             key={index}
-            className="flex flex-col bg-gradient-light-violet items-center rounded-2xl p-6 shadow-xl max-w-lg h-[250px] mx-4"
+            className="flex flex-col hover:bg-light-violet text-center bg-gradient-light-violet items-center rounded-2xl p-6 shadow-xl max-w-lg h-[220px] mx-4"
           >
-            <header className="text-white font-inandan text-3xl font-extrabold mb-4">
+            <header className="text-white font-inandan text-3xl font-medium mb-4">
               {rule.heading}
             </header>
-            <article className="text-white font-gill_sans font-medium text-lg overflow-auto">
+            <article className="text-white font-gill_sans font-base text-lg overflow-auto">
               {rule.content}
             </article>
           </div>
