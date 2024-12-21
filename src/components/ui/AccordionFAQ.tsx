53a727c728a0d0dfb8481270bd04c6a192b4ced6
maeEsp <maemae.espera@gmail.com>
2024-12-21 18:54:05 +0800

    feat: added images to the Accordion

--- src/components/ui/AccordionFAQ.tsx
+++ src/components/ui/AccordionFAQ.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import * as Accordion from '@radix-ui/react-accordion';
 import { BiSolidRightArrow } from 'react-icons/bi';
+import Image from 'next/image';
+import locationData from '@/data/locationsFAQ.json';
 
 type AccordionComponentProps = {
   question: string;
@@ -31,10 +33,10 @@ const AccordionComponent: React.FC<AccordionComponentProps> = ({
         <div className="transition-all duration-300">
           <Accordion.Header>
             <Accordion.Trigger
-              className={`flex justify-between items-center w-full p-2 text-left cursor-pointer bg-white text-dark-violet transition-colors duration-300 hover:text-white hover:bg-gradient-to-r hover:from-white hover:via-light-violet hover:to-white ${isAccordionOpen ? 'bg-gradient-to-r from-white via-light-violet to-white text-white' : ''}`}
+              className={`flex justify-between items-center w-full h-8 md:h-10 p-2 text-left cursor-pointer text-dark-violet transition-colors duration-300 hover:text-white hover:bg-gradient-to-r hover:from-white hover:via-light-violet hover:to-white ${isAccordionOpen ? 'bg-gradient-to-r from-white via-light-violet to-white text-white' : ''}`}
               onClick={handleAccordionClick}
             >
-              <span className="ml-4 pr-2 flex-1 text-center text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl ">
+              <span className="ml-10 pr-2 flex-1 text-center text-base sm:text-lg">
                 {question}
               </span>
               <BiSolidRightArrow
@@ -43,14 +45,33 @@ const AccordionComponent: React.FC<AccordionComponentProps> = ({
             </Accordion.Trigger>
           </Accordion.Header>
           <Accordion.Content
-            className={`overflow-hidden transition-all duration-300
-              ${isAccordionOpen ? 'max-h-screen py-4 animate-fadeIn' : 'max-h-0 py-0 opacity-0 animate-fadeOut'}`}
+            className={`overflow-hidden transition-all duration-300 ${isAccordionOpen ? 'max-h-screen py-4 animate-fadeIn' : 'max-h-0 py-0 opacity-0 animate-fadeOut'}`}
           >
-            <div>
-              <p className="px-4 text-dark-violet text-center text-xs sm:text-sm md:text-base lg:text-lg">
-                {answer}
-              </p>
-            </div>
+            {answer === 'location' ? (
+              <div className="flex flex-col sm:flex-row items-center sm:items-start px-4">
+                {locationData.map((location, index) => (
+                  <div key={index} className="flex flex-col items-center">
+                    <Image
+                      src={location.image}
+                      alt={location.title}
+                      width={100}
+                      height={100}
+                      className="w-28 h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 xl:w-60 xl:h-60 object-cover"
+                      unoptimized
+                    />
+                    <p className="py-4 text-dark-violet text-center text-xs lg:text-base">
+                      {location.title}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div>
+                <p className="px-4 text-dark-violet text-center text-xs lg:text-base">
+                  {answer}
+                </p>
+              </div>
+            )}
           </Accordion.Content>
         </div>
       </Accordion.Item>
