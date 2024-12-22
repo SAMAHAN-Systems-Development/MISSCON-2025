c4cedd4c5a10393916383b0cff5f3efd9634e359
Miraii133 <jiyovalmoria@gmail.com>
2024-12-22 15:51:54 +0800

    chore: reverted page.tsx back to original code, but added new workflow file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import CountdownTimer from '@/components/Countdown';
-import Footer from '@/components/Footer';
-import SlideMenu from '@/components/SlideMenu';
-import AccordionComponent from '@/components/ui/AccordionFAQ';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -14,9 +10,8 @@ export default function Home() {
   return (
     <main>
       {/* add sections here */}
-      <div>
-        <CountdownTimer />
-      </div>
+      The official Mindanao International Studies Society Convention (MISSCON)
+      2025 Website.
     </main>
   );
 }
