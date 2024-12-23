42849b8bc699d854d8108e17363b89c1affbe506
John Kyle Lampa <jklampa31@gmail.com>
2024-12-23 21:52:51 +0800

    chore: added footer and slide menu to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css';
 import { abel, adamina, gill_sans, pirata_one, inandan } from '@/styles/fonts';
+import Footer from '@/components/Footer';
+import SlideMenu from '@/components/SlideMenu';
 
 export default function RootLayout({
   children,
@@ -11,7 +13,11 @@ export default function RootLayout({
       <body
         className={`${abel.variable} ${adamina.variable} ${gill_sans.variable} ${pirata_one.variable} ${inandan.variable} antialiased`}
       >
-        {children}
+        <div className="relative min-h-screen">
+          <SlideMenu />
+          <div className="relative z-10">{children}</div>
+          <Footer />
+        </div>
       </body>
     </html>
   );
