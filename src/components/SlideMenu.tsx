d30f64f9b10b83a05b863a9d89c495ecf204ed8c
John Kyle Lampa <jklampa31@gmail.com>
2024-12-23 21:54:15 +0800

    fix: fixed position of slide menu and added feature to close slide menu without clicking icon

--- src/components/SlideMenu.tsx
+++ src/components/SlideMenu.tsx
@@ -16,7 +16,7 @@ export default function SlideMenu() {
   return (
     <>
       <div
-        className="w-full h-[787px] transition-all duration-300"
+        className="w-full h-screen transition-all duration-300 fixed top-0 left-0 z-50"
         style={
           openSlideMenu
             ? {
@@ -34,14 +34,17 @@ export default function SlideMenu() {
         </div>
 
         {openSlideMenu && (
-          <div>
-            <MenuNavButtons href="" text="About"></MenuNavButtons>
-            <MenuNavButtons href="" text="Registration"></MenuNavButtons>
-            <MenuNavButtons href="" text="Program Flow"></MenuNavButtons>
-            <MenuNavButtons href="" text="Speakers"></MenuNavButtons>
-            <MenuNavButtons href="" text="House Rules"></MenuNavButtons>
-            <MenuNavButtons href="" text="FAQs"></MenuNavButtons>
-            <MenuNavButtons href="" text="The Team"></MenuNavButtons>
+          <div className="flex">
+            <div className="w-4/5">
+              <MenuNavButtons href="" text="About"></MenuNavButtons>
+              <MenuNavButtons href="" text="Registration"></MenuNavButtons>
+              <MenuNavButtons href="" text="Program Flow"></MenuNavButtons>
+              <MenuNavButtons href="" text="Speakers"></MenuNavButtons>
+              <MenuNavButtons href="" text="House Rules"></MenuNavButtons>
+              <MenuNavButtons href="" text="FAQs"></MenuNavButtons>
+              <MenuNavButtons href="" text="The Team"></MenuNavButtons>
+            </div>
+            <div onClick={onMenuButtonClick} className="w-1/5"></div>
           </div>
         )}
       </div>
