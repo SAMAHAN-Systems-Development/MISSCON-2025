0e33ecde02958dba2ff5dc087cf6e4a815e95d2d
John Kyle Lampa <jklampa31@gmail.com>
2024-12-26 17:12:56 +0800

    fix: minor css changes

--- src/components/section/AboutCountdown.tsx
+++ src/components/section/AboutCountdown.tsx
@@ -28,8 +28,8 @@ export default function AboutCountdown() {
         </h1>
         <Image src={MissconLogo} alt={'foreground'} height={130} width={130} />
 
-        <div className="justify-items-center mt-28 md:mt-44 w-full sm:w-5/6 lg:w-4/6 font-gill_sans">
-          <p className='mb-8 text-base md:text-lg lg:text-2xl text-center font-gill_sans'>
+        <div className="flex flex-col items-center mt-28 md:mt-44 w-full sm:w-5/6 lg:w-4/6 font-gill_sans">
+          <p className="mb-8 text-base md:text-lg lg:text-2xl text-center font-gill_sans">
             The study of the international in Mindanao thrives and continues to
             integrate nascent transboundary trends into our understanding of the
             world. Imitating the complexity of indigenous textile patterns,
