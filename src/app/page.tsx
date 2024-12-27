fcf5fca770fe7dc10b3390ce08a78b22f9784ea0
John Kyle Lampa <jklampa31@gmail.com>
2024-12-27 11:31:04 +0800

    chore: added eventdaydetails component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import EventDayDetails from '@/components/EventDayDetails';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -12,6 +13,8 @@ export default function Home() {
       {/* add sections here */}
       The official Mindanao International Studies Society Convention (MISSCON)
       2025 Website.
+      <EventDayDetails dayNumber="1"></EventDayDetails>
+      <EventDayDetails dayNumber="2"></EventDayDetails>
     </main>
   );
 }
