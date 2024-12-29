e465ed34d3b88a37398e3e86f8d75a03e3a086cf
Ninya <mnamparaiso@addu.edu.ph>
2024-12-29 15:43:48 +0800

    style: change in the background.

--- src/app/house-rules/page.tsx
+++ src/app/house-rules/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 import RuleCards from '@/components/ui/RuleCards';
 import Image from 'next/image';
-import BackgroundImage from '../../../public/images/House-Rules-bg-final.png';
+import BackgroundImage from '../../../public/images/house-rules-bg.png';
+import bg from '../../../public/images/BG.png';
 
 export default function HouseRules() {
   return (
@@ -12,8 +13,24 @@ export default function HouseRules() {
           src={BackgroundImage}
           alt="background"
           fill
-          className="object-cover sm:object-fill"
+          className="object-cover"
         />
+      </div>
+      {/* Foreground Image Section */}
+      <div className="relative w-full h-36 overflow-hidden">
+        <Image
+          fill
+          src={bg}
+          className="absolute object-cover object-top rotate-180 scale-150 opacity-40 mt-[-28px] xl:mt-[-80px] 2xl:mt-[-28px]"
+          alt={'foreground'}
+        />
+        <div
+          className="absolute inset-0 opacity-50"
+          style={{
+            background:
+              'linear-gradient(to bottom, rgb(88,71,186) 1%, transparent)',
+          }}
+        ></div>
       </div>
       {/* Content */}
       <div className="relative z-10 flex flex-col text-center gap-y-10 sm:gap-y-14 lg:gap-y-20 justify-items-center pt-48 pb-96 px-4 sm:px-8">
@@ -23,6 +40,15 @@ export default function HouseRules() {
         <div className="px-4 sm:px-8 lg:px-20">
           <RuleCards />
         </div>
+      </div>
+      {/* Foreground Image Section */}
+      <div className="relative w-full h-36 overflow-hidden">
+        <Image
+          alt={'foreground'}
+          fill
+          src={bg}
+          className="absolute object-cover object-top opacity-40 scale-150 mt-7 xl:mt-20 2xl:mt-7"
+        />
       </div>
     </div>
   );
