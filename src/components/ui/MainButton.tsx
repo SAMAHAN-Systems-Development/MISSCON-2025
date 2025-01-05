1f4c0a1fcec8ed5289a82c05599bf4c26b34b01c
John Kyle Lampa <jklampa31@gmail.com>
2025-01-06 00:30:12 +0800

    style: reduced vertical padding

--- src/components/ui/MainButton.tsx
+++ src/components/ui/MainButton.tsx
@@ -12,7 +12,7 @@ const MainButton: React.FC<MainButtonProps> = ({ href, text }) => {
       href={href}
       className="bg-gradient-to-r from-violet to-purple
       hover:from-dark-violet hover:to-dark-violet text-white font-pirata_one text-3xl 
-      sm:text-4xl lg:text-5xl px-8 sm:px-12 lg:px-16 py-4 sm:py-5 lg:py-6 rounded-xl 
+      sm:text-4xl lg:text-5xl px-8 sm:px-12 lg:px-16 py-3 sm:py-4 lg:py-5 rounded-xl 
       sm:rounded-2xl shadow-md hover:shadow-lg transition-all duration-500 ease-in-out"
     >
       {text || 'register here'}
