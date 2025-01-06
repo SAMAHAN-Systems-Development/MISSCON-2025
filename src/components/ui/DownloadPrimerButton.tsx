00870fe12ff4da1df1891e8b6a2cfd1f2861ae60
Arabella Mejorada <arabella.mejorada@gmail.com>
2025-01-07 00:03:08 +0800

    feat: added functionality for download primer btn

--- src/components/ui/DownloadPrimerButton.tsx
+++ src/components/ui/DownloadPrimerButton.tsx
@@ -3,22 +3,18 @@
 import React from 'react';
 import { IoMdDownload } from 'react-icons/io';
 
-interface DownloadPrimerButtonProps {
-  onClick?: () => void;
-}
-
-const DownloadPrimerButton: React.FC<DownloadPrimerButtonProps> = ({
-  onClick,
-}) => {
-  const handleClick = () => {
-    if (onClick) {
-      onClick();
-    }
+const DownloadPrimerButton: React.FC = () => {
+  const handleDownload = () => {
+    const fileUrl = '/Event-Primer-MISSCON-2025.pdf';
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = 'Event-Primer-MISSCON-2025.pdf'; // File name when downloaded
+    link.click();
   };
 
   return (
     <button
-      onClick={handleClick}
+      onClick={handleDownload}
       className={`
         relative flex items-center gap-1 
         text-white font-light font-abel tracking-wider uppercase
