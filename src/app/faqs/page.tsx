afdee58c83f6a91c04a39c020262d9f9aea28d4b
John Kyle Lampa <jklampa31@gmail.com>
2025-01-09 01:05:32 +0800

    fix: fixed faqs bg

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -10,7 +10,7 @@ const FaqPage = () => {
       className="relative w-full h-full min-h-screen"
       style={{
         backgroundImage: `url(${bg})`,
-        backgroundSize: '400%',
+        backgroundSize: '500%',
         backgroundPosition: 'top',
         backgroundRepeat: 'no-repeat',
       }}
