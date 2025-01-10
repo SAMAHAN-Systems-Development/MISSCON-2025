bb2530ae7ec0a75aea028ebfab62fb8f859578b8
John Kyle Lampa <jklampa31@gmail.com>
2025-01-11 00:16:31 +0800

    fix: enlarge dev cards

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -6,53 +6,38 @@ interface CardProps {
   name: string;
   position: string;
   event: string;
-  type?: string;
 }
 
-export default function Card({
-  imageUrl,
-  name,
-  position,
-  event,
-  type,
-}: CardProps) {
+export default function Card({ imageUrl, name, position, event }: CardProps) {
   return (
     <>
       <div className="flex flex-col items-center w-min h-min hover:scale-105 transition-all duration-300">
         <div
-          className={`flex justify-center items-center ${type === 'developer' ? 'rounded-[10px]' : 'rounded-[7px]'} xsm:rounded-[10px] lg:rounded-xl  xl:rounded-2xl ${type === 'developer' ? 'p-[5px]' : 'p-1'} xsm:p-[5px] smd:p-[6px] lg:p-[7px] xl:p-2 w-min h-min bg-gradient-to-b from-red to-violet`}
+          className={`flex justify-center items-center rounded-xl lg:rounded-xl  xl:rounded-2xl p-[6px] lg:p-[7px] xl:p-2 w-min h-min bg-gradient-to-b from-red to-violet`}
         >
           <div
-            className={`relative ${type === 'developer' ? 'w-[130px]' : 'w-24'} xsm:w-[130px] smd:w-[160px] lg:w-48 xl:w-64 ${type === 'developer' ? 'h-40' : 'h-[120px]'} xsm:h-40 smd:h-48 lg:h-60 xl:h-[306px] ${type === 'developer' ? 'rounded-[8px]' : 'rounded-[5px]'} xsm:rounded-[8px] xl:rounded-[10px] overflow-hidden`}
+            className={`relative w-48 lg:w-48 xl:w-64 h-60 lg:h-60 xl:h-[306px] rounded-[8px] xl:rounded-[10px] overflow-hidden`}
           >
             <Image
               draggable={false}
               fill
               src={imageUrl}
-              className="rounded-card object-cover object-center"
+              className="rounded-card object-cover object-center scale-110 -mt-3"
               alt="/"
             />
           </div>
         </div>
 
-        <div
-          className={`font-gill_sans text-dark-violet ${type === 'developer' ? 'w-[130px]' : 'w-24'} xsm:w-[130px] smd:w-[160px] lg:w-48 xl:w-64 mt-2`}
-        >
+        <div className={`font-gill_sans text-dark-violet w-48 xl:w-64 mt-2`}>
           <p
-            className={`${type === 'developer' ? 'text-sm' : 'text-[10px]'} xsm:text-sm smd:text-lg lg:text-xl xl:text-2xl font-bold leading-tight smd:leading-6 xl:leading-8 xl:mt-1`}
+            className={`text-lg lg:text-xl xl:text-2xl font-bold leading-tight xl:leading-8 xl:mt-1`}
           >
             {name}
           </p>
-          <p
-            className={`${type === 'developer' ? 'text-xs' : 'text-[10px]'}  xsm:text-xs smd:text-base xl:text-lg leading-tight smd:leading-6 mt-1 xl:mt-0`}
-          >
+          <p className={`text-sm  xl:text-lg leading-tight mt-1 xl:mt-0`}>
             {position}
           </p>
-          <p
-            className={`${type === 'developer' ? 'text-xs' : 'text-[10px]'} xsm:text-xs smd:text-base xl:text-lg italic`}
-          >
-            {event}
-          </p>
+          <p className={`text-sm xl:text-lg italic`}>{event}</p>
         </div>
       </div>
     </>
